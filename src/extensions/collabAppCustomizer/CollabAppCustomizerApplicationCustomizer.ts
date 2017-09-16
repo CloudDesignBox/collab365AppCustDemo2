d17fa874ffda60cd7378553551204b5897cb02f0
Tony Phillips <Tony.Phillips@clouddesignbox.co.uk>
2017-09-16 11:11:22 +0100

    TypeScript and SASS added

--- src/extensions/collabAppCustomizer/CollabAppCustomizerApplicationCustomizer.ts
+++ src/extensions/collabAppCustomizer/CollabAppCustomizerApplicationCustomizer.ts
@@ -1,12 +1,20 @@
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
 import {
-  BaseApplicationCustomizer
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName
 } from '@microsoft/sp-application-base';
 
 import * as strings from 'CollabAppCustomizerApplicationCustomizerStrings';
 
 const LOG_SOURCE: string = 'CollabAppCustomizerApplicationCustomizer';
+
+//import styles as module
+import styles from './mystyles.module.scss';
+
+//not recommended
+//require('./badstyles.scss');
 
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
@@ -22,18 +30,46 @@ export interface ICollabAppCustomizerApplicationCustomizerProperties {
 export default class CollabAppCustomizerApplicationCustomizer
   extends BaseApplicationCustomizer<ICollabAppCustomizerApplicationCustomizerProperties> {
 
+    //placeholder
+    private _headerPlaceholder: PlaceholderContent | undefined;
+
   @override
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = '(No properties were provided.)';
-    }
-
-    alert(`Hello from ${strings.Title}:\n\n${message}`);
+    // Call render method for generating the needed html elements
+    this._renderPlaceHolders();
 
     return Promise.resolve<void>();
+  }
+
+  private _renderPlaceHolders(): void {
+    //see which placeholders are available
+    console.log('loading placeholders');
+    console.log('Available placeholders: ',
+    this.context.placeholderProvider.placeholderNames.map(name => PlaceholderName[name]).join(', '));
+
+    // Handling the top placeholder
+    if (!this._headerPlaceholder) {
+      this._headerPlaceholder =
+        this.context.placeholderProvider.tryCreateContent(
+          PlaceholderName.Top,
+          { //on dispose method
+          });
+          this._headerPlaceholder.domElement.innerHTML = `
+          <div class="${styles.myheader}">
+            <img src="https://clouddesignboxdev.sharepoint.com/sites/Team/SiteAssets/Collab365.png" alt="Collab365 Logo" />
+              <div class="${styles.socialmediaholder}">
+              <div class="${styles.socialtileholder}"><a class="${styles.socialtile} ${styles.facebook}" href="https://www.facebook.com"></a></div>
+              <div class="${styles.socialtileholder}"><a class="${styles.socialtile} ${styles.twitter}" href="https://www.twitter.com"></a></div>
+              <div class="${styles.socialtileholder}"><a class="${styles.socialtile} ${styles.linkedin}" href="https://www.linkedin.com"></a></div>
+              <div class="${styles.socialtileholder}"><a class="${styles.socialtile} ${styles.youtube}" href="https://www.youtube.com"></a></div>
+              <div class="${styles.socialtileholder}"><a class="${styles.socialtile} ${styles.instagram}" href="https://www.instagram.com"></a></div>
+            </div>
+          </div>
+          `;
+    }
+
   }
 }
 /**
